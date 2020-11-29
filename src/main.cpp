9555b94e71508b050aaa8d58170068bdba31b087
Dierk Arp <Dierk.Arp@messring.de>
2020-11-29 14:19:20 +0100

    initial checkin of naked main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+/**
+**********************************************************************************************************************************************************************************************************************************
+* @file:	main.cpp
+*
+* @brief:	Startup for Modbus Gateway
+*
+* @author:	Dierk Arp
+* @date:	20201129 16:01:24 
+* @version:	1.0
+*
+* @copyright:	(c)2020 HAHIS
+*
+* The reproduction, distribution and utilization of this document
+* as well as the communication of its content to others without
+* express authorization is prohibited. Offenders will be held liable
+* for the payment of damages. All rights reserved in the event of
+* the grant of a patent, utility model or design
+* Refer to protection notice ISO 16016
+*
+**********************************************************************************************************************************************************************************************************************************
+**/
+static const char TAG[] = __FILE__;
+
+
+#include <Arduino.h>
+
+void setup() 
+{
+  
+  
+  // put your setup code here, to run once:
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+}
